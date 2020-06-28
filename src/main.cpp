e2d3f380665266cc0180a09f7965b2272856e744
Florian Siegel <florian1siegel@googlemail.com>
2020-06-28 13:15:12 +0200

    Setup wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,49 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 #define SERIAL_BAUD 115200
-#define WIFI_SSID "Mann im Mond"
-#define WIFI_PASSWORD "WarumBinIchSoFreohlich??"
+#define WIFI_SSID 
+#define WIFI_PASSWORD 
+
+const char* ssid = "Mann im Mond";
+const char* password = "WarumBinIchSoFroehlich??";
+
+void setupWifi() {
+  uint8_t dotCount = 0;
+  wl_status_t status = WL_DISCONNECTED;
+  do {
+    switch (status) {
+      case WL_DISCONNECTED:
+      case WL_CONNECT_FAILED:
+        Serial.println("Reconnect");
+        WiFi.begin(ssid, password);
+      default:
+        break;
+    }
+    delay(500);
+    Serial.print("connecting .");
+    for (uint8_t count = 0; count < dotCount; count++) {
+      Serial.print(".");
+    }
+    Serial.println("");
+
+    if (dotCount > 5) {
+      dotCount = 0;
+    } else {
+      dotCount++;
+    }
+    status = WiFi.status();
+    Serial.print(status);
+  } while (status != WL_CONNECTED);
+ 
+  Serial.print("Connected - IP:");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   Serial.println("Hello");
+  setupWifi();
   // put your setup code here, to run once:
 }
 
